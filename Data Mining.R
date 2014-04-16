52664b94d1366c2526c4fadb0118cc844e716a2a
donlabin <donlabin@gmail.com>
2014-04-16 18:28:08 +0800

    先將一些Data Mining會用到的package放上去

---
+++ Data Mining.R
@@ -0,0 +1,32 @@
+# Clustering --------------------------------------------------------------
+library(fpc)
+library(cluster)
+library(pvclust)
+library(mclust)
+
+# Classification ----------------------------------------------------------
+library(rpart)
+library(party)
+library(randomForest)
+library(tree)
+library(maptree)
+library(survival)
+
+# Association Rules -------------------------------------------------------
+library(arules)
+library(drm)
+
+# Sequential Patterns -----------------------------------------------------
+library(arulesSequences)
+
+# Time Series Analysis ----------------------------------------------------
+library(timsac)
+
+# Text Mining -------------------------------------------------------------
+
+
+# Statistical Analysis ----------------------------------------------------
+library(nlme)
+
+
+
