f097507342ffbe7da738014026f0115c647c6b20
donlabin <donlabin@gmail.com>
2014-04-16 17:20:02 +0800

    2014/04/16:還沒有推論分析

---
+++ explorary analysis.R
@@ -0,0 +1,20 @@
+# 針對資料做初步的探索 --------------------------------------------------------------
+names()
+str()
+attributes()
+class()
+summary()
+mean()
+sd()
+quantile()
+
+# 可視化分析 -------------------------------------------------------------------
+# 利用lattice包
+library(lattice)
+stripplot()
+densityplot()
+bwplot()
+xyplot()
+histogram()
+# 利用ggplot2包
+library(ggplot2)
