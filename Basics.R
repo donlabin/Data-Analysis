a3eb5c34ff9dd67eb5c8fc6ab56f9cdcc6fc2a10
unknown <E123188557@SKEA7341.skl.com.tw>
2015-06-26 17:15:57 +0800

    test 2015-06-25

---
+++ Basics.R
@@ -0,0 +1,11 @@
+demo(graphics) # show the beautiful graphs in R
+rm(list=ls(all=TRUE)) # remove all objects
+ls() #show all objects
+options(digits=22) # 7 is the default value in R
+getwd() #get your current working dictionary
+setwd() #set the working dictionary you want
+save.image() #save working space to R.data
+library(car) #include the package to your working space
+detach() #exclude a database or package
+.packages(all.available=TRUE) #秀出有哪些已下載哪些package
+some() #隨機看看對象中的一些內容
