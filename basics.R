8e48b9e4f912c41d030cc6578ca97fd96d5f631c
unknown <E123188557@SKEA7341.skl.com.tw>
2015-06-26 17:31:56 +0800

    新增一些尚未分類的內容

--- basics.R
+++ basics.R
@@ -89,4 +89,16 @@ lines(height,22.5*height^2,col=3)
   [1] 1 2
 
 
-  
+# 2015-06-25 新增，但尚未分類 -----------------------------------------------------
+
+demo(graphics) # show the beautiful graphs in R
+rm(list=ls(all=TRUE)) # remove all objects
+ls() #show all objects
+options(digits=22) # 7 is the default value in R
+getwd() #get your current working dictionary
+setwd() #set the working dictionary you want
+save.image() #save working space to R.data 
+library(car) #include the package to your working space
+detach() #exclude a database or package
+.packages(all.available=TRUE) #秀出有哪些已下載哪些package
+some() #隨機看看對象中的一些內容
